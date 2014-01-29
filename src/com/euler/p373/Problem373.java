1821302452a42b58d428ca26b402c2d20fe6eb29
Jon Schneider <jkschneider@gmail.com>
2014-01-28 20:52:11 -0600

    Two brute force solutions to 373

--- src/com/euler/p373/Problem373.java
+++ src/com/euler/p373/Problem373.java
@@ -6,29 +6,23 @@ import static java.lang.Math.max;
 
 import static java.lang.Math.sqrt;
 
+// brute force
 public class Problem373 {
 	public static void main(String[] args) {
-		int N = 100;
+		int N = 1200;
 		int sum = 0;
 
 		double rad = 0.0;
-		for (int c = 1; c < N*2; c++) {
-			for (int b = c; b < N*2; b++) { // b >= a to avoid duplicate
-				int a;
+		for (long c = 1; c < N*2; c++) {
+			for (long b = c; b < N*2; b++) { // b >= a to avoid duplicate
+				long a;
 				for (a = max(c, b); a < b + c; a++) { // c must satisfy triangle inequality
 					rad = radius(a, b, c);
 					if (rad > N)
 						break;
 					
-					if (floor(rad) == rad && rad <= N) {
-						System.out.print(c + ", " + b + ", " + a + " = ");
-						System.out.println(rad);
+					if (floor(rad) == rad && rad <= N && rad > 0) {
 						sum += rad;
-					}
-					else if(rad - floor(rad) < 0.0000001) {
-						System.out.print("CLOSE: ");
-						System.out.print(c + ", " + b + ", " + a + " = ");
-						System.out.println(rad);
 					}
 				}
 			}
@@ -37,7 +31,7 @@ public class Problem373 {
 		System.out.println("sum = " + sum);
 	}
 
-	private static double radius(int a, int b, int c) {
+	private static double radius(long a, long b, long c) {
 		return (a * b * c) / sqrt((a + (b + c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - c)));
 	}
 }
--- src/com/euler/p373/Problem373_2.java
+++ src/com/euler/p373/Problem373_2.java
@@ -1,6 +1,10 @@
 package com.euler.p373;
 
-import static com.euler.MathExt.*;
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+import static java.lang.Math.sqrt;
+
+import com.euler.MathExt;
 
 public class Problem373_2 {
 	public static void main(String[] args) {
@@ -8,24 +12,32 @@ public class Problem373_2 {
 	}
 
 	public void run() {
-		int N = 25;
-		
-		for(int k = 1; k < N; k++) {
-			for(int n = 1; n < N; n++) {
-				for(int m = n; m < N; m++) {
-					if(relativelyPrime(m,n,k) && m*n > k*k && k*k > (m*m*n)/(2*m+n)) {
-						int a = n*(m*m+(k*k));
-						int b = m*(n*n+(k*k));
-						int c = (m+n)*(m*n-(k*k));
-						
-						System.out.println(a + ", " + b + ", " + c);
+		int sum = 0;
+
+		int N = 1200;
+		for(long a = 1; a <= 2*N; a++) {
+			for(long b = (int) ceil((((double) a)+1)/2); b <= a; b++) {
+				for(long c = a+1-b; c <= b; c++) {
+					double r = (a*b*c) / sqrt((a+b+c)*(a+b-c)*(a-b+c)*(-a+b+c));
+					if(floor(r) == r) {
+						if(r <= N && r > 0) {
+//							System.out.println(a + ", " + b + ", " + c + " = " + r);
+//							System.out.println("\t\tgcd(a,b,c) = " + gcd(a,b,c));
+							sum += r;
+						}
 					}
 				}
 			}
 		}
+		
+		System.out.println("sum = " + sum);
+	}
+	
+	int gcd(long a, long b, long c) {
+		return (int) MathExt.gcd(MathExt.gcd(a,b), MathExt.gcd(b,c));
 	}
 	
 	boolean relativelyPrime(int m, int n, int k) {
-		return gcd(m,n) == 1 && gcd(n,k) == 1 && gcd(m,k) == 1;
+		return MathExt.gcd(m,n) == 1 && MathExt.gcd(n,k) == 1 && MathExt.gcd(m,k) == 1;
 	}
 }
