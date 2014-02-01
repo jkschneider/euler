a15c2b4d47f436d4abf2ddc68088d0c396ace797
Jon Schneider <jkschneider@gmail.com>
2014-02-01 17:33:01 -0600

    Solved 69

---
+++ src/com/euler/p69/Problem69.java
@@ -0,0 +1,65 @@
+package com.euler.p69;
+
+import static com.euler.MathExt.gcd;
+import static com.euler.PrimeTest.prime;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Problem69 {
+	int N = 1000000;
+	
+	Map<Integer, Integer> totientCache = new HashMap<Integer, Integer>();
+	
+	int totient(int n) {
+		if(n == 1 || n == 2) return 1;
+		Integer totient = totientCache.get(n);
+		
+		if(totient == null) {
+			if(n % 2 == 0) {
+				if(n/2 % 2 == 0)
+					totient = 2*totient(n/2);
+				else
+					totient = totient(n/2);
+			}
+			else if(prime(n))
+				totient = n-1;
+			else {
+				// phi(i*j) = phi(i)*phi(j)*(d/phi(d)) where d = gcd(i,j)
+				int i = firstFactor(n);
+				int j = n/i;
+				int d = (int) gcd(i,j);
+				totient = (totient(i)*totient(j)*d)/totient(d);
+			}
+			
+			totientCache.put(n, totient);
+		}
+		return totient;
+	}
+	
+	int firstFactor(int n) {
+		for (int i = 2; i <= Math.sqrt(n); i++)
+			if(n % i == 0)
+				return i;
+		return n;
+	}
+
+	void run() {
+		int maxN = 0;
+		double max = 0;
+		
+		for(int n = 1; n <= N; n++) {
+			double totN = totient(n);
+			if(n/totN > max) {
+				max = n/totN;
+				maxN = n;
+			}
+		}
+		
+		System.out.println("solution = " + maxN);
+	}
+	
+	public static void main(String[] args) {
+		new Problem69().run();
+	}
+}
--- util/com/euler/PrimeFactorGenerator.java
+++ util/com/euler/PrimeFactorGenerator.java
@@ -7,7 +7,7 @@ public class PrimeFactorGenerator {
     public static List<Integer> primeFactors(int number) {
         int n = number; 
         List<Integer> factors = new ArrayList<Integer>();
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             while (n % i == 0) {
                 factors.add(i);
                 n /= i;
